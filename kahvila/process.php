40de2b59cece919a0ccb6e4ec0dcf2918746c5e1
projektitiimi1 <84176057+projektitiimi1@users.noreply.github.com>
2021-10-27 15:06:24 +0300

    Update process.php

--- kahvila/process.php
+++ kahvila/process.php
@@ -1,3 +1,23 @@
+<?
+require_once('config.php');
+?>
 <?php
 
-echo "Hello from process";
+if(isset($_POST)) {
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $password = sha1($_POST['password']);
+
+            $sql = "INSERT INTO users (firstname, lastname, email, password ) VALUES(?,?,?,?)";
+            $stminsert = $db->prepare($sql);
+            $result = $stmtinsert->execute([$firstname, $lastname, $email, $password]);
+            if($result) {
+                echo'Successfully saved.';
+            }else {
+                echo 'There were errors while saving the data';
+            }
+
+        }else{
+          echo 'No data';
+}
